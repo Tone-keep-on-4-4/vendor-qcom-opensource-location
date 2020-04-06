f2335063dfb813bbe35f034868f4a4cca7f8085e
sjllls <2012ji123@gmail.com>
2020-04-06 08:11:20 +0800

    Android.mk: Build for all

--- Android.mk
+++ Android.mk
@@ -1,11 +1,1 @@
-ifeq ($(PRODUCT_PLATFORM_SOD),true)
-ifeq ($(TARGET_KERNEL_VERSION), 4.9)
-LOCAL_PATH := $(call my-dir)
-include $(LOCAL_PATH)/build/target_specific_features.mk
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
-endif
-
-ifneq ($(filter rhine shinano,$(PRODUCT_PLATFORM)),)
 include $(call all-subdir-makefiles)
-endif
